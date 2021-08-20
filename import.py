1e9bf2dda103064d6492bfa6611f76b680f33964
glendah-moraa <moraaglendah@gmail.com>
2021-08-20 22:09:29 +0300

    changed

---
+++ import.py
@@ -0,0 +1,27 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+
+st.write('''
+# Topic
+
+## Subtopic
+
+Description
+''')
+
+
+
+tickerSymbol = "GOOGL"
+
+tickerData = yf.Ticker(tickerSymbol)
+
+ticherdf = tickerData.history(Period = 'id', start = '2010-5-31', end = '2020-5-31')
+
+
+st.write('1. Closing price')
+st.line_chart(ticherdf.Close)
+
+st.write('2. The volume')
+st.line_chart(ticherdf.Volume)
